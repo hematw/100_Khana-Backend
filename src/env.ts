76d76fe9da537043f94f49bfb291fea93fa3a3cd
Hemat Waziri <hematullahw@gmail.com>
2025-11-08 00:11:29 +0430

    chore: added message to env file

--- src/env.ts
+++ src/env.ts
@@ -6,13 +6,23 @@ dotEnvConfig();
 
 const env = createEnv({
   server: {
-    MONGO_STRING: z.string().min(1),
-    PORT: z.number().min(1),
-    MY_JWT_SECRET: z.string().min(1),
+    MONGO_STRING: z
+      .string()
+      .min(1, { message: "❌ Missing MONGO_STRING in environment variables" }),
+    PORT: z
+      .preprocess(
+        (val) => Number(val),
+        z
+          .number()
+          .min(1, { message: "❌ PORT must be a valid number greater than 0" })
+      ),
+    MY_JWT_SECRET: z
+      .string()
+      .min(1, { message: "❌ Missing MY_JWT_SECRET in environment variables" }),
   },
   runtimeEnv: {
     MONGO_STRING: process.env.MONGO_STRING,
-    PORT: Number(process.env.PORT),
+    PORT: process.env.PORT,
     MY_JWT_SECRET: process.env.MY_JWT_SECRET,
   },
 });
